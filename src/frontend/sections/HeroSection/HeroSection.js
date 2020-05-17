5d6524b07832f03e0c2de037a623c111290f5cbf
Rafael Caetano <>
2020-05-17 18:14:29 +0200

    Styling | HeroSection

--- src/frontend/sections/HeroSection/HeroSection.js
+++ src/frontend/sections/HeroSection/HeroSection.js
@@ -1,10 +1,21 @@
 import React from "react";
-import style from "./heroSection.scss";
+import style from "./heroSection.module.scss";
 
-function HeroSection() {
+function HeroSection({ image = null, title = null, label = null }) {
+  //const imageUrl = image || null; // TODO: Use this once DB is finished
+  const imageUrl = "/images/tuist1.png";
+  title = "Tuna Universitária do Instituto Superior Técnico";
+  label = "Vem descobrir";
+
   return (
-    <section className={style.heroSection}>
-      <p>THIS IS A section</p>
+    <section
+      className={style.heroSection}
+      style={{ background: imageUrl ? `url(${imageUrl})` : null }}
+    >
+      <div className={style.wrapper}>
+        {title && <h1 className={style.title}>{title}</h1>}
+        {label && <p className={style.label}>{label}</p>}
+      </div>
     </section>
   );
 }
